053b0ea6caeecd99891945beea708ea9b99b1bee
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-09-27 13:48:24 +0600

    api intigration st

--- app/Register/page.jsx
+++ app/Register/page.jsx
@@ -21,6 +21,15 @@ const page = () => {
       
     })
     
+
+
+    // -----------api frtching--------
+    const url = 'https://api.freeapi.app/api/v1/users/register';
+const options = {
+  method: 'POST',
+  headers: {accept: 'application/json', 'content-type': 'application/json'},
+  body: '{"email":"user.email@domain.com","password":"test@123","role":"ADMIN","username":"doejohn"}'
+};
     
     // ----------error State-----
     
